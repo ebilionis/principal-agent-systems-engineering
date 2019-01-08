d0eaeffc1a33b0ddec4debf57fd3a8ba3aee6b0a
Ilias Bilionis <ebilionis@gmail.com>
2019-01-08 11:35:42 -0500

    Added a requirement based transfer function.

---
+++ sepdesign/_transfer_functions.py
@@ -0,0 +1,99 @@
+"""
+Transfer functions.
+
+"""
+
+
+__all__ = ['TransferFunction']
+
+
+import theano
+import theano.tensor as T
+from _function import Function
+
+
+class TransferFunction(Function):
+    """
+    An abstract class for transfer functions.
+
+    :param t_q: The symbolic variable for quality.
+    :param num_a:   The number of parameters (immutable).
+    :param t_a: The symbolic variable for the parameters.
+    :param t_t: The symbolic variable for the transfer function.
+    """
+
+    def __init__(self, t_q, num_a, t_a, t_t):
+        self._num_a = num_a
+        super(TransferFunction, self).__init__([t_q, t_a], t_t)
+
+    @property
+    def num_a(self):
+        """
+        Get the number of parameters.
+        """
+        return self._num_a
+
+    @property
+    def t_q(self):
+        """
+        Get the symbolic variable for the quality.
+        """
+        return self.t_x[0]
+
+    @property
+    def t_a(self):
+        """
+        Get the symbolic variable for the effort.
+        """
+        return self.t_x[1]
+
+    @property
+    def t_t(self):
+        """
+        Get the symbolic variable for the transfer function.
+        """
+        return self.t_t
+
+
+class RequirementTransferFunction(TransferFunction):
+    """
+    A transfer function that pays a fixed amount if a requirement is met.
+
+    :param req: The requirement (immutable, default one).
+    :param t_q: The symbolic variable for quality (created if None)
+    :param t_a: The symbolic variable for the parameters (created if None)
+    :param gamma:   The step function is represented by a sigmoid. This
+                    parameter gives the sharpness of the sigmoid. (immutable)
+    """
+
+    def __init__(self, req=1.0, t_q=None, t_a=None, gamma=10.):
+        self._req = req
+        self._gamma = gamma
+        if t_q is None:
+            t_q = T.dscalar('q')
+        if t_a is None:
+            t_a = T.dvector('a')
+        t_t = t_a[0] / (1. + T.exp(gamma * (req - t_q)))
+        super(RequirementTransferFunction, self).__init__(t_q, 1, t_a, t_t)
+
+    @property
+    def req(self):
+        """
+        Get the requirement.
+        """
+        return self.req
+
+    @property
+    def gamma(self):
+        """
+        Get the gamma parameter.
+        """
+        return self._gamma
+
+
+if __name__ == '__main__':
+    tr = RequirementTransferFunction()
+    tr.compile()
+    a = [0.3]
+    for q in [0.1, 0.6, 0.8, 1., 1.2]:
+        print 'tr(%1.2f, [%1.2f]) = %1.2f' % (q, a[0], tr(q, a))
