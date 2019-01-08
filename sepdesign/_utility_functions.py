5df838872482d7a8d07bd4855bc778afaac30feb
Ilias Bilionis <ebilionis@gmail.com>
2019-01-08 15:10:06 -0500

    Updated utility functions.

--- sepdesign/_utility_functions.py
+++ sepdesign/_utility_functions.py
@@ -40,7 +40,7 @@ class ExponentialUtilityFunction(UtilityFunction):
     def __init__(self, t_pi=None, risk_preference=0.0):
 
         if t_pi == None:
-            t_pi = T.dscalar('pi')
+            t_pi = T.dvector('pi')
         if risk_preference != 0.0:
             a = 1.0 / (1.0 - T.exp(risk_preference))
             b = a
@@ -55,7 +55,7 @@ class ExponentialUtilityFunction(UtilityFunction):
 if __name__ == '__main__':
     import numpy as np
     import matplotlib.pyplot as plt
-    pi = np.linspace(0,1, 100)
+    pis = np.linspace(0,1, 100)
     u_averse  = ExponentialUtilityFunction(risk_preference = -2.0)
     u_neutral = ExponentialUtilityFunction(risk_preference =  0.0)
     u_prone   = ExponentialUtilityFunction(risk_preference =  2.0)
@@ -64,17 +64,13 @@ if __name__ == '__main__':
     u_neutral.compile()
     u_prone.compile()
 
-    ua = []
-    un = []
-    up = []
-    for i in pi:
-        ua += [u_averse(i)]
-        un += [u_neutral(i)]
-        up += [u_prone(i)]
+    ua = u_averse(pis)
+    un = u_neutral(pis)
+    up = u_prone(pis)
 
-    plt.plot(pi, ua, label='risk averse')
-    plt.plot(pi, un, label='risk neutral')
-    plt.plot(pi, up, label='risk prone')
+    plt.plot(pis, ua, label='risk averse')
+    plt.plot(pis, un, label='risk neutral')
+    plt.plot(pis, up, label='risk prone')
 
     plt.xlabel(r'$\pi$')
     plt.ylabel(r'utility')
