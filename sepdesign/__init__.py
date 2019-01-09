802f9342927fb0e8da86fb0c8861aa35f5f3afea
Ilias Bilionis <ebilionis@gmail.com>
2019-01-08 21:05:31 -0500

    Validating the derivatives. Missing constrained cases.

--- sepdesign/__init__.py
+++ sepdesign/__init__.py
@@ -8,4 +8,6 @@ TODO SALAR: WRITE
 from _function import *
 from _cost_functions import *
 from _quality_functions import *
+from _transfer_functions import *
+from _utility_functions import *
 from _types import *
