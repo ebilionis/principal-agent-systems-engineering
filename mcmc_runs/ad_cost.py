83d3af8e827dd662de5fa44a7b2678c997669291
Ilias Bilionis <ebilionis@gmail.com>
2019-02-25 15:23:17 -0500

    Small changes.

--- mcmc_runs/ad_cost.py
+++ mcmc_runs/ad_cost.py
@@ -16,18 +16,19 @@ import mpi4py.MPI as mpi
 
 class SteadyPaceSMC(ps.SMC):
 
-    def _find_next_gamma(self, gamma):
-        self._loglike = self._get_loglike(self.gamma, gamma)
-        return gamma
+    pass
+    #def _find_next_gamma(self, gamma):
+    #    self._loglike = self._get_loglike(self.gamma, gamma)
+    #    return gamma
 
 
 def make_model():
     agent_type11 = AgentType(LinearQualityFunction(1.6, 0.1),
-                            QuadraticCostFunction(0.04),
+                            QuadraticCostFunction(0.1),
                             ExponentialUtilityFunction(-2.0))
 
     agent_type12 = AgentType(LinearQualityFunction(1.6, 0.1),
-                            QuadraticCostFunction(0.1),
+                            QuadraticCostFunction(0.2),
                             ExponentialUtilityFunction(-2.0))
     agents = Agent([agent_type11, agent_type12])
     t = RequirementPlusIncentiveTransferFunction(gamma=30.)
@@ -47,43 +48,50 @@ def make_model():
         f = res['exp_u_pi_0']
         # The constraints that must be positive
         g = res['exp_u_pi_agents']
+        type_1 = a[:4]
+        type_2 = a[4:]
 
-        g[0] += [p._irc[0][0].evaluate(a[:4])['exp_u_pi_e_star']-\
-              p._irc[0][0].evaluate(a[4:])['exp_u_pi_e_star']]
-
-        g[0] += [p._irc[0][1].evaluate(a[4:])['exp_u_pi_e_star']-\
-              p._irc[0][1].evaluate(a[:4])['exp_u_pi_e_star']]
-
+        g[0] += [g[0][0] - p._irc[0][0].evaluate(type_2)['exp_u_pi_e_star']]
+        g[0] += [g[0][1] - p._irc[0][1].evaluate(type_1)['exp_u_pi_e_star']]
 
         return np.hstack([[f], g[0]])
-    @pm.deterministic
-    def results(a=a):
-        return p.evaluate(a)
+
+    #@pm.deterministic
+    #def results(a=a):
+    #    return p.evaluate(a)
     
     @pm.stochastic(dtype=float, observed=True)
     def loglike(value=1.0, fg=fg, gamma=gamma):
         f = fg[0]
         g = fg[1]
         return gamma * f + \
-                np.sum(np.log(1.0 / (1.0 + np.exp(-gamma**2 * g))))
+                np.sum(np.log(1.0 / (1.0 + np.exp(- gamma * g))))
     return locals()
 
 
 if __name__ == '__main__':
     model = make_model()
     mcmc = pm.MCMC(model)
-    mcmc.use_step_method(ps.RandomWalk, model['a'])
-    smc = SteadyPaceSMC(mcmc, num_particles=300, num_mcmc=1, verbose=4,
+    mcmc.use_step_method(ps.RandomWalk, model['a'], proposal_sd=0.01)
+    smc = SteadyPaceSMC(mcmc, num_particles=100, num_mcmc=100, verbose=4,
                  gamma_is_an_exponent=True,
                  ess_reduction=0.9, adapt_proposal_step=True,
                  mpi=mpi)
-    smc.initialize(0.01)
-    for gamma in np.linspace(.0, 50.0, 800)[1:]:
+    smc.initialize(0.1)
+    for gamma in np.linspace(0.1, 10, 10)[1:]:
         smc.move_to(gamma)
         pa = smc.get_particle_approximation().gather()
         if mpi.COMM_WORLD.Get_rank() == 0:
             idx = np.argmax(pa.fg[:, 0])
             print('max f = ', pa.fg[idx, 0], 'g = ', pa.fg[idx, 1:])
+            g = pa.fg[idx, 1:]
+            print('\tType 1\tType 2')
+            g00 = g[0]
+            g01 = -(g[2] - g00)
+            g11 = g[1]
+            g10 = -(g[3] - g11)
+            print('Type 1\t%1.3f\t%1.3f' % (g00, g01))
+            print('Type 2\t%1.3f\t%1.3f' % (g10, g11))
             print('> ', pa.a[idx, :])
 
     if mpi.COMM_WORLD.Get_rank() == 0:
@@ -91,7 +99,7 @@ if __name__ == '__main__':
         print()
         print('*'*30,' final results ','*'*30)
         print()
-        print('final results: ', pa.results[idx])
+        #print('final results: ', pa.results[idx])
         print('parameters: ', pa.a[idx, :])
         print()
         print('max f = ', pa.fg[idx, 0], 'g = ', pa.fg[idx, 1:])
